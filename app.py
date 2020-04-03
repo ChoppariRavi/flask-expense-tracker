8f20e9e31aa53662de3f6b65b0e199fe1624efe3
ChoppariRavi <choppari.ravi@gmail.com>
2020-04-03 13:03:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def homepage():
     return render_template('hello.html', name=name)
 
-# if __name__ == '__main__':
-#     app.run(debug=True, use_reloader=True)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
     
