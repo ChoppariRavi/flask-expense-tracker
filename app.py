8b7e91d1fc65acc4a94ac662b4650c926f401b39
ChoppariRavi <choppari.ravi@gmail.com>
2020-04-08 22:38:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from datetime import datetime
 from flask import render_template
-app = Flask(__name__)
+
+app = Flask(__name__,
+            static_folder = "./client/dist/static",
+            template_folder = "./client/dist")
 
 @app.route('/')
 def homepage():
-    return render_template("hello.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
